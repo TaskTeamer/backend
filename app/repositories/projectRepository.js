9fe850306cbf37209d4a459f73bc2b6aeede0d53
Mustafa Enes Tepe <72195347+MET-DEV@users.noreply.github.com>
2022-12-18 15:17:59 +0300

    project return type changed

--- app/repositories/projectRepository.js
+++ app/repositories/projectRepository.js
@@ -1,7 +1,7 @@
 const db = require("../config/db")
 
 const getByUserId=async function(userid){
-    return await (await db.query("select team_users.user_id, projects.name,projects.team_id from team_users Inner Join teams on team_users.team_id=teams.id Inner Join projects on projects.team_id=teams.id where user_id="+userid)).rows
+    return await (await db.query("select projects.id,team_users.user_id, users.user_name,projects.name,projects.team_id from team_users Inner Join teams on team_users.team_id=teams.id Inner Join projects on projects.team_id=teams.id inner join users on team_users.user_id=users.id where user_id="+userid)).rows
 }
 const saveProject=async(project)=>{
 
